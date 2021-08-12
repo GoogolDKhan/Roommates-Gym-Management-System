1d58153158e8bae321e07f419000954599001a85
Sarfaraz <khansarfaraz2141@gmail.com>
2021-08-12 20:17:33 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,50 @@
+roommates = {1: "Sarfaraz", 2: "Sashant", 3: "Prabhjot"}
+log = {1: "Exercise", 2: "Diet"}
+
+
+def getdate():
+    import datetime
+    return datetime.datetime.now()
+
+
+try:
+    print("Select your name:")
+    for key, value in roommates.items():
+        print(f"Press {key} for {value}")
+    roommate = int(input())
+
+    print(f"\nSelected roommate is {roommates[roommate]}")
+
+    print("Press 1 for Log")
+    print("Press 2 for Retrieve")
+    num = int(input())
+
+    if num == 1:
+        for key, value in log.items():
+            print(f"Press {key} to log {value}")
+        log_num = int(input())
+        print(f"Selected Job : {log[log_num]}")
+        f = open(roommates[roommate] + "_" + log[log_num] + ".txt", "a")
+        k = 'y'
+        while(k != "n"):
+            print(f"Enter {log[log_num]}")
+            mytext = input()
+            f.write("[ " + str(getdate()) + " ] : " + mytext + "\n")
+            k = input("ADD MORE ? y/n:")
+            continue
+        f.close()
+
+    elif num == 2:
+        for key, value in log.items():
+            print(f"Press {key} to retrieve {value}")
+        log_num = int(input())
+        print(f"{roommates[roommate]}-{log[log_num]} Report :")
+        f = open(roommates[roommate] + "_" + log[log_num] + ".txt")
+        contents = f.readlines()
+        for line in contents:
+            print(line, end="")
+        f.close()
+    else:
+        print("Invalid Input !!!")
+except Exception as e:
+    print("Wrong Input !!!")
